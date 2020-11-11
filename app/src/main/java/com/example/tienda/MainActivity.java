5ac66dad9b4dcd4bdcf1961e8a0de5d62bdfa696
Diego Cárdenas <diegocardenasc@outlook.es>
2020-11-10 19:19:25 -0500

    asignar valor a num

--- app/src/main/java/com/example/tienda/MainActivity.java
+++ app/src/main/java/com/example/tienda/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //comentario
         int num;
+
+        //comentario#2
+        num =10;
     }
 }
